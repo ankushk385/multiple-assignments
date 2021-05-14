c3b6138f59a3002e8120bff4695c74f0704c03ee
Goldy <goldy@192.168.1.9>
2021-05-14 16:20:40 +0530

    using useeffect

--- src/components/UserForm-UseEffect-Dependencies/UseForm.jsx
+++ src/components/UserForm-UseEffect-Dependencies/UseForm.jsx
@@ -3,13 +3,53 @@ import {useForm} from 'react-hook-form';
 import './styles.css'
 const UseForm = () => {
 
-let statesIndia = ["punjab","delhi","jammu"];
-let statesUsa = ["new york","new jersey"];
-let citiesJammu = ["gandhinagar","lal chownk"];
-let citiesPunjab = ["mohali","pathankot"]
-let citiesDelhi = ["new delhi","lajpathnagar"]
-let citiesnewYork = ["los santos","vice city","sanandreas"]
-let citiesnewJersey = ["newark","hampshire"]
+    const list = [
+        {
+            country: 'India', states: [
+                {
+                    name: 'Punjab', cities: [
+                        'Mohali', 'Jalandhar'
+                    ]
+                },
+                {
+                    name: 'Delhi', cities: [
+                        'lajpatnagar', 'delhi-6'
+                    ]
+                }
+            ]
+        },
+        {
+            country: 'Usa', states: [
+                {
+                    name: 'Chicago', cities: [
+                        'fargo', 'dallas'
+                    ]
+                },
+                {
+                    name: 'Texas', cities: [
+                        'austin', 'housten'
+                    ]
+                }
+            ]
+        }
+    ]
+
+    const getCountry = (data)=>{
+        let arr = []
+       data.map((value)=>(
+        arr.push(value.country)
+       ));
+       setCountryOptions(arr)
+    }
+  
+
+const [countryOptions, setCountryOptions] = useState([])
+const [stateOptions, setStateOptions] = useState([])
+const [cityOptions, setCityOptions] = useState([])
+
+const [country, setCountry] = useState()
+const [state, setState] = useState()
+const [city, setCity] = useState()
 
 const {register,handleSubmit,watch} = useForm();
 
@@ -18,22 +58,30 @@ const selectedCurrentState = watch("state",false)
 const selectedCurrentCity = watch("city",false)
 
 
+
+useEffect(()=>{
+    getCountry(list)
+setCountry(selectedCurrentCountry);
+}
+,[country])
+
 const onSubmit = (data) => {
     alert(JSON.stringify(data));
   };
 
-console.log(selectedCurrentCountry);
-console.log(selectedCurrentState);
-console.log(selectedCurrentCity);
-
+  
     return (
         <div  className="form-container">
            <form onSubmit={handleSubmit(onSubmit)}>
                <div className="country-select">
            <select  {...register('country')}  id="country" name="country" >
            <option >...select country...</option>
-               <option value="india" name="india">INDIA</option>
-               <option value="usa"  name="usa">USA</option>
+               {
+                   countryOptions.map((value)=>(
+                       <option value={value.country}>{value.country}</option>
+                   ))
+               
+               }
            </select>
            </div>
            
@@ -41,91 +89,14 @@ console.log(selectedCurrentCity);
             <div className="state-select">
                  <select {...register('state')} id="state" name="state">
                  <option name={false} value={false}>...select state</option>
-                    {
-                        selectedCurrentCountry==='india' ? (statesIndia.map((key)=>(
-                            <option name={key} value={key} >{key}</option>
-                        ))) : (statesUsa.map((key)=>(
-                            <option name={key} value={key} >{key}</option>
-                        )))
-                        
-                    }
+                   
                     
                 </select>
             </div>
         ) 
     }
     
-    {selectedCurrentState && (
-                        <div className="city-select">
-                            <select {...register('city')} name="city">
-                               
-                                {(() => {
-
-switch (selectedCurrentState) {
-
-   case 'punjab':
-
-       return (
-
-        citiesPunjab.map((key)=>(
-            <option name={key} value={key} >{key}</option>
-            ))
-
-       )
-
-   case 'delhi':
-
-       return (
-
-        citiesDelhi.map((key)=>(
-            <option name={key} value={key} >{key}</option>
-            ))
-
-       )
-
-       case 'jammu':
-
-        return (
- 
-            citiesJammu.map((key)=>(
-                <option name={key} value={key} >{key}</option>
-                ))
- 
-        )
-        case 'new york':
-
-        return (
- 
-            citiesnewYork.map((key)=>(
-                <option name={key} value={key} >{key}</option>
-                ))
- 
-        )
-        case 'new jersey':
-
-        return (
- 
-            citiesnewJersey.map((key)=>(
-                <option name={key} value={key} >{key}</option>
-                ))
- 
-        )
-
-   default:
-
-       return (
-
-        <option value={false} name={false}>...select city</option>
-
-       )
-}
-})()}
-                  
-                            </select>
-                        </div>
-                            
-                        )
-                    }
+  
         <button type="submit">submit</button>
         </form>
        
